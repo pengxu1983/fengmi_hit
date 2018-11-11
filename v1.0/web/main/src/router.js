a4e49a812d5830df652d88d722575fca3731266a
pengxu1983@163.com <pengxu1983@163.com>
2018-11-11 16:13:49 +0800

    mv from v3.0 tree

--- v1.0/web/main/src/router.js
+++ v1.0/web/main/src/router.js
@@ -1,23 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Create from './views/Create.vue'
+import Usermain from './views/Usermain.vue'
+import Admin from './views/Admin.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/',
+      name: 'home',
+      component: Login
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: Create
+    },
+    {
+      path: '/usermain',
+      name: 'usermain',
+      component : Usermain
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      props: true,
+      component : Admin
+    },
+    //{
+    //  path: '/about',
+    //  name: 'about',
+    //  // route level code-splitting
+    //  // this generates a separate chunk (about.[hash].js) for this route
+    //  // which is lazy-loaded when the route is visited.
+    //  component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    //}
   ]
 })
